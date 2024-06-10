f69ef699b319b2a8390ee00d90b181b929b8c65f
kmacek <kmacek@sygic.com>
2024-06-10 18:24:50 +0200

    initial commit

---
+++ app/src/main/java/com/macek/contacts/ui/favorites/FavoritesScreen.kt
@@ -0,0 +1,26 @@
+package com.macek.contacts.ui.favorites
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.macek.contacts.ui.compose.ContactsTheme
+
+@Composable
+fun FavoritesScreen() {
+    ContactsTheme {
+        Surface {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(text = "Favorites screen")
+            }
+        }
+    }
+}
